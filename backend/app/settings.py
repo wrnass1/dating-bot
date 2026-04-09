00d1256c60bc3210f601b4a548e1ea3bde4eae12
Анастасия Тищенко <pipupa@MacBook-Pro-6.local>
2026-04-09 12:31:46 +0300

    added gitignore

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -3,9 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
-
     database_url: str
-
     api_host: str = "0.0.0.0"
     api_port: int = 8000
 
